78ff36af5a843d34d4325bb507fb673d314eeaf0
Unknown <maxingham@gmail.com>
2018-03-27 13:39:35 +1100

    Fixed url returning to previous when closing overlay

--- js/index.js
+++ js/index.js
@@ -1,73 +1,79 @@
 var clicked = false; //start off with overlay off
 
-//load up Join Us content on button click
-function joinClick(file) {
-    if (clicked == true){
-        console.log("clicked off");
+//Close overlay function
+function closeOverlay() {
+    if (clicked == true) {
         $('.overlay').removeClass('overlayOn');
         clicked = false;
+        history.back();
     }
-    else{
-        console.log("clicked on");
+};
+
+//load up Join Us content on button click
+function joinClick(file) {
+    if (clicked == true) {
+        closeOverlay();
+    } else {
         $('.overlay').addClass('overlayOn');
         clicked = true;
-        $.ajax({url: "/html/ajax/" + file, success: function(result){
-            $("#overlayContent").html(result);
-        }});
-        history.pushState(null, null, '#hello'); //set url extension
+        $.ajax({
+            url: "/html/ajax/" + file,
+            success: function (result) {
+                $("#overlayContent").html(result);
+            }
+        });
+        history.pushState(null, null, '#JoinUs'); //set url extension
     }
 };
 
 //Load up staff profile when clicking on their photo
 function staffClick(staff) {
-    if (clicked == true){
-        console.log("clicked off");
-        $('.overlay').removeClass('overlayOn');
-        clicked = false;
-    }
-    else{
-        console.log("clicked on");
+    if (clicked == true) {
+        closeOverlay();
+    } else {
         $('.overlay').addClass('overlayOn');
         clicked = true;
-        $.ajax({url: "/html/ajax/staff/" + staff + ".html", success: function(result){
-            $("#overlayContent").html(result);
-        }});
+        $.ajax({
+            url: "/html/ajax/staff/" + staff + ".html",
+            success: function (result) {
+                $("#overlayContent").html(result);
+            }
+        });
         history.pushState(null, null, '#' + staff); //set url extension
     }
 };
 
 
-//Close overlay with button
+//Close overlay function
 function closeOverlay() {
-    if (clicked == true){
-        console.log("clicked off");
+    if (clicked == true) {
         $('.overlay').removeClass('overlayOn');
         clicked = false;
+        history.back();
     }
 };
 
 
 //Use ESC button for closing overlay
-$( document ).on( 'keydown', function ( e ) {
-    if ( e.keyCode === 27 ) { // ESC
-        $('.overlay').removeClass('overlayOn');
-        console.log("esc closed");
-        clicked = false;
-        window.history.pushState('forward', null, './#forward');
+$(document).on('keydown', function (e) {
+    if (e.keyCode === 27) { // ESC
+        closeOverlay();
     }
 });
 
 
 //Intercept back button for use with overlay
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     if (window.history && window.history.pushState) {
 
         window.history.pushState('forward', null, './#forward');
 
-        $(window).on('popstate', function() {
-            closeOverlay();
+        $(window).on('popstate', function () {
+            if (clicked == true) { //need to avoid an extra history.back so using unique closeOverlay
+                $('.overlay').removeClass('overlayOn');
+                clicked = false;
+            }
         });
-
     }
 });
