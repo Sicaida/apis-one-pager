dfbf6b6e40986b6d2ca93a68dc7ebdf82adc2f59
Unknown <maxingham@gmail.com>
2018-03-27 13:24:47 +1100

    Added ajax to import staff

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-var clicked = false;
-function joinClick() {
+var clicked = false; //start off with overlay off
+
+//load up Join Us content on button click
+function joinClick(file) {
     if (clicked == true){
         console.log("clicked off");
         $('.overlay').removeClass('overlayOn');
@@ -9,9 +11,33 @@ function joinClick() {
         console.log("clicked on");
         $('.overlay').addClass('overlayOn');
         clicked = true;
+        $.ajax({url: "/html/ajax/" + file, success: function(result){
+            $("#overlayContent").html(result);
+        }});
+        history.pushState(null, null, '#hello'); //set url extension
     }
 };
 
+//Load up staff profile when clicking on their photo
+function staffClick(staff) {
+    if (clicked == true){
+        console.log("clicked off");
+        $('.overlay').removeClass('overlayOn');
+        clicked = false;
+    }
+    else{
+        console.log("clicked on");
+        $('.overlay').addClass('overlayOn');
+        clicked = true;
+        $.ajax({url: "/html/ajax/staff/" + staff + ".html", success: function(result){
+            $("#overlayContent").html(result);
+        }});
+        history.pushState(null, null, '#' + staff); //set url extension
+    }
+};
+
+
+//Close overlay with button
 function closeOverlay() {
     if (clicked == true){
         console.log("clicked off");
@@ -20,27 +46,28 @@ function closeOverlay() {
     }
 };
 
+
+//Use ESC button for closing overlay
 $( document ).on( 'keydown', function ( e ) {
     if ( e.keyCode === 27 ) { // ESC
         $('.overlay').removeClass('overlayOn');
         console.log("esc closed");
         clicked = false;
+        window.history.pushState('forward', null, './#forward');
     }
 });
 
-//
-//var wow = new WOW(
-//    {
-//        boxClass:     'wow',      // animated element css class (default is wow)
-//        animateClass: 'animated', // animation css class (default is animated)
-//        offset:       0,          // distance to the element when triggering the animation (default is 0)
-//        mobile:       true,       // trigger animations on mobile devices (default is true)
-//        live:         true,       // act on asynchronously loaded content (default is true)
-//        callback:     function(box) {
-//            // the callback is fired every time an animation is started
-//            // the argument that is passed in is the DOM node being animated
-//        },
-//        scrollContainer: null // optional scroll container selector, otherwise use window
-//    }
-//);
-//wow.init();
+
+//Intercept back button for use with overlay
+jQuery(document).ready(function($) {
+
+    if (window.history && window.history.pushState) {
+
+        window.history.pushState('forward', null, './#forward');
+
+        $(window).on('popstate', function() {
+            closeOverlay();
+        });
+
+    }
+});
